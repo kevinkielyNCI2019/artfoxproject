b2479a7d3f43af32108eb1d195518c12e62bc60f
KevinKielyNCI2019 <kevinjohnkiely@gmail.com>
2019-10-25 20:24:52 +0000

    Artworks image uploader added

--- app/controllers/artworks_controller.rb
+++ app/controllers/artworks_controller.rb
@@ -82,6 +82,6 @@ class ArtworksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def artwork_params
-      params.require(:artwork).permit(:title, :cat, :desc, :price, :imageurl, :sold, :artist_id)
+      params.require(:artwork).permit(:title, :cat, :desc, :price, :artpic, :sold, :artist_id)
     end
 end
